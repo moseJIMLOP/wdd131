2fb815b03920c3ede5f404c441b7bbee8752f914
moseJIMLOP <mose.jimenez.lopez2002@gmail.com>
2025-11-08 11:17:38 -0600

    css

--- week02/scripts/temples.js
+++ week02/scripts/temples.js
@@ -4,3 +4,11 @@ currentYear.textContent = today.getFullYear();
 
 const lastModified = document.querySelector('#lastModified');
 lastModified.textContent = `Last Modified: ${document.lastModified}`;
+
+const menuButton = document.querySelector('#menuButton');
+const nav = document.querySelector('nav');
+
+menuButton.addEventListener('click', () => {
+    nav.classList.toggle('open');
+    menuButton.classList.toggle('open');
+});
