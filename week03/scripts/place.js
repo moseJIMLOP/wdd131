ec85e88504750f4162d968961cf33e74c03195fb
moseJIMLOP <mose.jimenez.lopez2002@gmail.com>
2025-11-19 21:59:17 -0600

    weather

--- week03/scripts/place.js
+++ week03/scripts/place.js
@@ -3,29 +3,21 @@ document.getElementById("year").textContent = new Date().getFullYear();
 document.getElementById("lastModified").textContent = 
   document.lastModified;
 
-const temperature = 10;
-const windSpeed = 10; 
-const condition = "Cloudy";
+const temperature = 25;
+const windSpeed = 15; 
+const condition = "Sunny";
 
 function calculateWindChill(t, v) {
-  return (
-    13.12 +
-    0.6215 * t -
-    11.37 * Math.pow(v, 0.16) +
-    0.3965 * t * Math.pow(v, 0.16)
-  );
+  return 
+    13.12 + 0.6215 * t - 11.37 * Math.pow(v, 0.16) + 0.3965 * t * Math.pow(v, 0.16);
 }
-let windChillText;
+let windChillText = "N/A";
 
 if (temperature <= 10 && windSpeed > 4.8) {
 
   const wc = calculateWindChill(temperature, windSpeed);
   windChillText = wc.toFixed(1) + " °C";
-} else {
-  windChillText = "N/A";
-}
+} 
 
-document.getElementById("temp").textContent = temperature + " °C";
-document.getElementById("wind").textContent = windSpeed + " km/h";
 document.getElementById("condition").textContent = condition;
 document.getElementById("Chill").textContent = windChillText;
