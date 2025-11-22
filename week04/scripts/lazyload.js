5253eaf36d4ea01b4aa6d6505317118fbb31160e
moseJIMLOP <mose.jimenez.lopez2002@gmail.com>
2025-11-22 01:53:15 -0600

    gatitos

---
+++ week04/scripts/lazyload.js
@@ -0,0 +1,5 @@
+const currentYear = new Date().getFullYear();
+document.getElementById('year').textContent = currentYear;
+
+const lastModified = document.lastModified;
+document.getElementById('lastModified').textContent = lastModified;
